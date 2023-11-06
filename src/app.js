a6c52248e7839dab4aec347ca379b1bac4d91692
Somuya Khandelwal <141111077+somuyakhandelwal@users.noreply.github.com>
2023-11-06 11:56:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ class Player {
     // Progession 1: Create member variables and assign values
     this.id = id;
     this.name = name;
-    this.image = './images/super-' + (id + 1) + '.png';
+    this.image = 'images/super-' + (id + 1) + '.png';
     this.strength = this.getRandomStrength();
     this.type = type;
     this.selected = false;
